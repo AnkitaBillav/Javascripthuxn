327dfa1db3c41d888d5ec168f674967cf543dcbc
Ankita <ankitabillav@gmail.com>
2025-01-16 22:34:14 +0530

    Initial commit

---
+++ huxn/JSdemos/js.js
@@ -0,0 +1,349 @@
+// // // console; //object has many methods
+// // // console.log(100 + 200);
+// // // console.warn(" this is a warning , this is the most important warning!!");
+// // // console.table({ name: "huxn webdev" });
+// // console.log(
+// //   " hero is huxn who is best in webdev and i am going to follow my sir and be a javascript mosnter !!!"
+// // );
+
+// // console.log(
+// //   " hero is huxn who is best in webdev and i am going to follow my sir and be a javascript mosnter !!!"
+// // );
+
+// // console.log(
+// //   " hero is huxn who is best in webdev and i am going to follow my sir and be a javascript mosnter !!!"
+// // );
+
+// // console.log(
+// //   " hero is huxn who is best in webdev and i am going to follow my sir and be a javascript mosnter !!!"
+// // );
+
+// // console.log(
+// //   " hero is huxn who is best in webdev and i am going to follow my sir and be a javascript mosnter !!!"
+// // );
+
+// // console.clear();
+// // console.log(
+// //   "hey world its me the developer Ankita for u be better now , i am gonna change the whole coding world by making the coding super easy and suimple for the world , i will build arounf 10000 technologies for sure from now, srill i am in babay steps aand leanring very simple synatx and gonna  be a javascript monster or  mern stack  monster "
+// // );
+
+// // console.clear();
+// // //clge or log for console.log shortcuts now !!
+// //variables
+
+// // box to store data
+// //pseudocode:doesnt work but code for image or anything
+// //(reserved keyword) (variable name) (type of value)
+// //var outdated
+// //const , let = new
+// // let name = "Ankita";
+// // let watdowanttobecomeinlife = "coder";
+// // let gender = "female";
+// // let twitter = "@ankita";
+// // console.log(
+// //   name + " " + gender + " " + watdowanttobecomeinlife + " " + twitter
+// // );
+// //primitive data types
+
+// // let first = 20;
+// // let second = 10;
+
+// // console.log(first + second);
+// // console.log(first + second);
+// // console.log(first - second);
+
+// // console.log(first * second);
+
+// // console.log(first / second);
+
+// // console.log(first % second);
+// // console.log(first ** second);
+// // let num = 10;
+// // console.log(num + undefined);
+// // //falsy values to a boolean are
+// // // false,null,undefined,0,-0,NaN,'',"" ,``,empty quotes
+// // let notdefined;
+// // console.log(notdefined);
+// // let isJsprogramminlanguage = true;
+// // let ishard = false;
+// // console.log(isJsprogramminlanguage + " " + ishard);
+// // let favnum = 9;
+// // console.log(favnum + undefined);
+
+// // let num1 = 100;
+// // let num2 = "100";
+// // console.log(num1 === num2);
+// // console.log(num1 == num2);
+// // console.log(num1 != num2);
+// // console.log(num1 == num2);
+// // let favactorfirstname = "Ranbir";
+// // let favactorlastname = "kapoor";
+// // let fullname = favactorfirstname + favactorlastname;
+
+// // let uppercase = fullname.toUpperCase();
+
+// // let message = `my fav actor is ${uppercase}.and  he did great acting in barfi movie and APGK and YJHD `;
+
+// // console.log(message + `his best show is silicon valley in 2022`);
+
+// // console.log(fullname);
+// // console.log(message);
+// // console.log(uppercase);
+// // console.log(favactorfirstname);
+// // console.log(favactorlastname);
+
+// //loops and conditions !!
+
+// // let password;
+// // if (password === 8) {
+// //   console.log("welcome !");
+// // } else if (password < 8) {
+// //   console.log("password is too short ");
+// // } else if (password > 8) {
+// //   console.log("password is too long!");
+// // } else {
+// //   console.log("please provide a password !");
+// // }
+
+// // let day = "mon";
+// // switch (day) {
+// //   case "mon":
+// //     console.log("monday");
+// //     break;
+
+// //   case "tue":
+// //     console.log("tuesday");
+// //     break;
+
+// //   case "wed":
+// //     console.log("wednesday");
+// //     break;
+
+// //   default:
+// //     console.log(" noday");
+// // }
+
+// // let fruit = "banana";
+// // switch (fruit) {
+// //   case "banana":
+// //     console.log("banana is good !!");
+// //     break;
+// //   case "orange":
+// //     console.log("orange fan i am");
+// //     break;
+// //   case "apple":
+// //     console.log("how u like thgt fruits");
+// //     break;
+// //   default:
+// //     console.log(" never heard abt it ");
+// // }
+
+// // for (let i = 0; i <= 5; i++) {
+// //   console.log("-----outerloop------", i);
+// //   for (let j = 0; j <= 5; j++) {
+// //     console.log("----- the inner loop-------", j);
+// //   }
+// // }
+// // for (let i = 0; i <= 10; i++) {
+// //   console.log("outerloop", i);
+// //   for (let k = 0; k <= 5; k++) {
+// //     console.log("-----innerloop-----", k);
+// //   }
+// // }
+
+// // for (let i = 0; i <= 1000; i++) {
+// //   console.log(" Ankita ", i);
+// // }
+
+// // let i = 10;
+// // while (i <= 100) console.log("Ankita Billav ", i);
+// // i++;
+// //do while
+
+// // let i = 0;
+// // do {
+// //   console.log("hello world", i);
+// //   i++;
+// // } while (i <= 5);
+// // const favsingers = ["arijit", "sonu", "kk"];
+// // console.log(favsingers[0]);
+// // const favnumbers = [9, 8, 2, 10, 5];
+// // const mixarrays = [1, 2, 3, "hellopie", ["cycle", "cat"]];
+
+// // console.log(
+// //   mixarrays[0] + " " + mixarrays[1],
+// //   mixarrays[2],
+// //   mixarrays[3],
+// //   mixarrays[4][0],
+// //   mixarrays[4][1]
+// // );
+// // const fruits = [
+// //   "apple",
+// //   "banana",
+// //   "chikoo",
+// //   "grapes",
+// //   "pineapple",
+// //   "kiwi",
+// //   "litchi",
+// // ];
+// // const nums = [9, 4, 0, 2, 6, 1];
+// // // console.log(fruits.includes("greenapples"));
+// // // console.log(fruits.join("---"));
+// // // console.log(fruits.reverse());
+// // console.log(fruits.slice(0, 3));
+// // console.log(nums.sort());
+
+// // console.log(fruits.pop());
+// // console.log(fruits.push("mangoes"));
+// // console.log(fruits.shift());
+// // console.log(fruits);
+// // console.log(fruits.unshift("strawberries"));
+// // console.log(fruits);
+// // const person = {
+// //   10: "ten",
+// // };
+// // console.log(person["10"]);
+// // const obj = {
+// //   color: "red",
+// //   name: "huxn",
+// //   age: 23,
+// // };
+// // console.log(obj["color"] + " " + obj["name"] + "  " + obj["age"]);
+
+// // person.lastname = "webdev";
+// // person.location = ["planet", "Earth"];
+// // console.log(person);
+
+// // console.log(delete person.lastname);
+// // console.log(person);
+
+// // const employee = {
+// //   name: "Ranveer singh",
+// //   age: 34,
+// //   married: true,
+// //   job: "Actor",
+// // };
+
+// // employee.wife = "Deepika";
+// // employee.kid = "Dua";
+// // console.log(delete employee.age);
+
+// // console.log(employee);
+
+// // const car = {
+// //   type: "suv",
+// //   model: "suv200",
+// //   color: "red",
+// // };
+// // console.log(typeof car);
+// // car.type = "Toyoto";
+// // console.log(car);
+
+// // car.wheels = 4;
+// // console.log(car);
+
+// // function greet() {
+// //   console.log(" hello from a function ");
+// // }
+// // greet();
+
+// function hello() {
+//   console.log(" this is a function in javascript !!");
+// }
+
+// hello();
+// hello();
+// hello();
+// hello();
+// hello();
+// hello();
+// hello();
+
+// //call, run , execute
+// function person(name) {
+//   console.log(` person name is ${name}`);
+// }
+
+// function sum(x, y) {
+//   return x + y;
+// }
+// const res = sum(200, 100);
+// const res1 = sum(11, 22);
+// const res2 = sum(200, 900);
+// const res3 = sum(200, 300);
+// console.log(res);
+// console.log(res1);
+
+// console.log(res2);
+// console.log(res3);
+
+// function keys(abc) {
+//   return abc;
+// }
+// const hey = keys(123);
+// const hey1 = keys(200);
+// const key3 = keys(2000);
+// const key4 = keys(34);
+// console.log(`hey===${hey}`);
+// console.log(`hey1===${hey1}`);
+
+// console.log(`key3===${key3}`);
+
+// console.log(`key4===${key4}`);
+
+// // console.log(hey);
+// function myFunction(a, b) {
+//   return a * b;
+// }
+// const ans = myFunction(10, 20);
+// console.log(ans);
+
+// function user(name) {
+//   return `name is ${name}`;
+// }
+// user("yoyo honey singh");
+
+// const answer = function (user) {
+//   console.log(`hello ${user}`);
+// };
+// answer("huxn");
+// onst maths = function (divide) {
+//   console.log(`lets didvider it  ${divide}`);
+// };
+// maths(123);
+// callback functions now in vs code
+
+function greet(name, flower) {
+  console.log(`this flower ${name}`);
+  flower();
+}
+function flower() {
+  console.log(" i love this flower!!");
+}
+greet("jasmine", flower);
+function hyderabad(place, food) {
+  console.log(`this place ${place} is awesome `);
+  food();
+}
+function food(place) {
+  console.log(`biryani  over this this
+     place is awesome !!`);
+}
+hyderabad("AnnandoBrahma", food);
+
+function numbers(a, b) {
+  console.log(a + b);
+}
+function sum(sumis, numbers) {
+  console.log(` sum of a and b is `);
+  numbers(1, 2);
+}
+sum(10, numbers);
+
+function showCallFunc(fn) {
+  const value = 10;
+  fn(value);
+}
+showCallFunc(function (value) {
+  console.log(value);
+});
